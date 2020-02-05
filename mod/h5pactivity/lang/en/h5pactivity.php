6b9b216ae9e065eeb0aeff1645c4cf1c0d4773ea
Ferran Recio <ferran@moodle.com>
2020-02-05 18:04:57 +0100

    MDL-67707 mod_h5pactivity: plugin skeleton

---
+++ mod/h5pactivity/lang/en/h5pactivity.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_h5pactivity
+ * @category    string
+ * @copyright   2020 Ferran Recio <ferran@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['areapackage'] = 'Package file';
+$string['displayexport'] = 'Allow H5P package download';
+$string['displayembed'] = 'Show embed button';
+$string['displaycopyright'] = 'Show copyright button (if defined in package)';
+$string['h5pactivity:addinstance'] = 'Add a new H5P';
+$string['h5pactivity:view'] = 'View H5P';
+$string['h5pactivityfieldset'] = 'H5P Settings';
+$string['h5pactivityname'] = 'H5P';
+$string['h5pactivitysettings'] = 'Settings';
+$string['h5pdisplay'] = 'H5P display settings';
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'H5P activity';
+$string['modulename_help'] = 'Use this module to use a H5P compatible content as a course activity.';
+$string['modulename_link'] = 'mod/h5pactivity/view';
+$string['modulenameplural'] = 'H5P activities';
+$string['nonewmodules'] = 'No H5P';
+$string['package'] = 'Package file';
+$string['package_help'] = 'The package file is a h5pfile containing H5P dynamic content.';
+$string['pluginadministration'] = 'H5P administration';
+$string['pluginname'] = 'H5P activity';
+$string['privacy:metadata'] = 'The File resource plugin does not store any personal data.';
+$string['view'] = 'View';
