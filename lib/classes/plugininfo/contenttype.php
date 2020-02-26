24e6cd065e2e5a6bb70201e82db564b6ddd22d3b
Sara Arjona <sara@moodle.com>
2020-02-26 13:12:37 +0100

    MDL-67795 contentbank: clean all the content when uninstalling plugin

--- lib/classes/plugininfo/contenttype.php
+++ lib/classes/plugininfo/contenttype.php
@@ -189,10 +189,8 @@ class contenttype extends base {
 
         $contents = $DB->get_records('contentbank_content', ['contenttype' => 'contenttype_'.$this->name]);
         foreach ($contents as $content) {
-            $fs = get_file_storage();
-            $fs->delete_area_files($content->contextid, 'contentbank', false, $content->id);
+            \core_contentbank\base::delete_content($content);
         }
-        $DB->delete_records('contentbank_content', ['contenttype' => 'contenttype_'.$this->name]);
 
         parent::uninstall_cleanup();
     }
