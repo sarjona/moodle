e0d34d236e48aacdbb5b085b0e48c7d45d7fef73
Sara Arjona <sara@moodle.com>
2021-02-25 17:21:32 +0100

    MDL-69331 core_contentbank: Hide disabled H5P content-types
    If a H5P content-type is disabled:
    - The content bank won't display existing contents having it as a
    main library.
    - The content bank won't allow to create new contents using it.

--- contentbank/contenttype/h5p/classes/content.php
+++ contentbank/contenttype/h5p/classes/content.php
@@ -36,4 +36,39 @@ use html_writer;
  */
 class content extends \core_contentbank\content {
 
+    /**
+     * Returns user has access permission for the content itself.
+     * If the H5P content-type library is disabled, the user won't have access to it.
+     *
+     * @return bool     True if content could be accessed. False otherwise.
+     */
+    public function is_view_allowed(): bool {
+        // Force H5P content to be deployed.
+        $fileurl = $this->get_file_url();
+        // Skip capability check when creating the H5P content (because it has been created by trusted users).
+        $h5pplayer = new \core_h5p\player($fileurl, new \stdClass(), true, '', true);
+        // Flush error messages.
+        $h5pplayer->get_messages();
+
+        // Check if the H5P entry has been created and if the main library is enabled.
+        $file = $this->get_file();
+        if (!empty($file)) {
+            $h5p = \core_h5p\api::get_content_from_pathnamehash($file->get_pathnamehash());
+            if (empty($h5p)) {
+                // If there is no H5P entry for this content, it won't be displayed unless the user has the manageanycontent
+                // capability. Reasons for contents without a proper H5P entry in DB:
+                // - Invalid H5P package (it won't be never deployed).
+                // - Disabled content-type library (it can't be deployed so there is no way to know the mainlibraryid).
+                $context = \context::instance_by_id($this->content->contextid);
+                if (!has_capability('moodle/contentbank:manageanycontent', $context)) {
+                    return false;
+                }
+            } else if (!\core_h5p\api::is_library_enabled((object) ['id' => $h5p->mainlibraryid])) {
+                // If the main library is disabled, it won't be displayed.
+                return false;
+            }
+        }
+
+        return parent::is_view_allowed();
+    }
 }
