a4c6763203915254c2ca16bf60b68915712d9c2b
Sara Arjona <sara@moodle.com>
2021-03-18 16:30:02 +0100

    MDL-67999 contentbank: Deprecate contentbank/upload.php
    As the form to upload files to the content bank has been moved to
    model, this file is not required.
    It has been deprecated and it will be removed in MDL-71146.

--- contentbank/upload.php
+++ contentbank/upload.php
@@ -24,102 +24,18 @@
 
 require('../config.php');
 
-use core\output\notification;
-
 require_login();
 
+debugging('Use of contentbank/upload.php have been deprecated. This page has been implemented using a modal form in index.php.',
+    DEBUG_DEVELOPER);
+
 $contextid = optional_param('contextid', \context_system::instance()->id, PARAM_INT);
-$context = context::instance_by_id($contextid, MUST_EXIST);
-
-$cb = new \core_contentbank\contentbank();
-if (!$cb->is_context_allowed($context)) {
-    print_error('contextnotallowed', 'core_contentbank');
-}
-
-require_capability('moodle/contentbank:upload', $context);
-
 $id = optional_param('id', null, PARAM_INT);
+$params = ['contextid' => $contextid];
 if ($id) {
-    $content = $cb->get_content_from_id($id);
-    $contenttype = $content->get_content_type_instance();
-    if (!$contenttype->can_manage($content) || !$contenttype->can_upload()) {
-        print_error('nopermissions', 'error', $returnurl, get_string('replacecontent', 'contentbank'));
-    }
-}
-
-$title = get_string('contentbank');
-\core_contentbank\helper::get_page_ready($context, $title, true);
-if ($PAGE->course) {
-    require_login($PAGE->course->id);
-}
-$returnurl = new \moodle_url('/contentbank/index.php', ['contextid' => $contextid]);
-
-$PAGE->set_url('/contentbank/upload.php');
-$PAGE->set_context($context);
-$PAGE->navbar->add(get_string('upload', 'contentbank'));
-$PAGE->set_title($title);
-$PAGE->set_heading($title);
-$PAGE->set_pagetype('contentbank');
-
-$maxbytes = $CFG->userquota;
-$maxareabytes = $CFG->userquota;
-if (has_capability('moodle/user:ignoreuserquota', $context)) {
-    $maxbytes = USER_CAN_IGNORE_FILE_SIZE_LIMITS;
-    $maxareabytes = FILE_AREA_MAX_BYTES_UNLIMITED;
-}
-
-if ($id) {
-    $extensions = $contenttype->get_manageable_extensions();
-    $accepted = implode(',', $extensions);
+    $params['id'] = $id;
+    $url = '/contentbank/view.php';
 } else {
-    $accepted = $cb->get_supported_extensions_as_string($context);
+    $url = '/contentbank/index.php';
 }
-
-$data = new stdClass();
-$options = array(
-    'subdirs' => 1,
-    'maxbytes' => $maxbytes,
-    'maxfiles' => -1,
-    'accepted_types' => $accepted,
-    'areamaxbytes' => $maxareabytes
-);
-file_prepare_standard_filemanager($data, 'files', $options, $context, 'contentbank', 'public', 0);
-
-$customdata = ['contextid' => $contextid, 'data' => $data, 'options' => $options, 'id' => $id];
-$mform = new \core_contentbank\form\upload_files(null, $customdata);
-
-$error = '';
-
-if ($mform->is_cancelled()) {
-    redirect($returnurl);
-} else if ($formdata = $mform->get_data()) {
-    require_sesskey();
-    // Get the file and create the content based on it.
-    $usercontext = \context_user::instance($USER->id);
-    $fs = get_file_storage();
-    $files = $fs->get_area_files($usercontext->id, 'user', 'draft', $formdata->file, 'itemid, filepath, filename', false);
-    if (!empty($files)) {
-        $file = reset($files);
-        if ($id) {
-            $content = $contenttype->replace_content($file, $content);
-        } else {
-            $content = $cb->create_content_from_file($context, $USER->id, $file);
-        }
-        $viewurl = new \moodle_url('/contentbank/view.php', ['id' => $content->get_id(), 'contextid' => $contextid]);
-        redirect($viewurl);
-    } else {
-        $error = get_string('errornofile', 'contentbank');
-    }
-}
-
-echo $OUTPUT->header();
-echo $OUTPUT->box_start('generalbox');
-
-if (!empty($error)) {
-    echo $OUTPUT->notification($error, notification::NOTIFY_ERROR);
-}
-
-$mform->display();
-
-echo $OUTPUT->box_end();
-echo $OUTPUT->footer();
+redirect((new \moodle_url($url, $params))->out(false));
