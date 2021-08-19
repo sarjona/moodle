3d779036251a7f38311c6c0eb92a804c2d9cc3d6
Sara Arjona <sara@moodle.com>
2021-08-19 17:18:07 +0200

    MDL-72112 admin_presets: Create settings classes
    Settings classes defined in lib/settings_types.php have been created
    as separated files in classes/local/setting.
    The admin_preset_setting class won't be abstract any more to use
    it as default class if there is no admin_setting_xxxx class.

---
+++ admin/tool/admin_presets/classes/local/setting/admin_preset_admin_setting_gradecat_combo.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_admin_presets\local\setting;
+
+use admin_setting;
+
+/**
+ * A select with force and advanced options
+ *
+ * @package          tool_admin_presets
+ * @copyright        2021 Pimenko <support@pimenko.com><pimenko.com>
+ * @author           Jordan Kesraoui | Sylvain Revenu | Pimenko based on David Monllaó <david.monllao@urv.cat> code
+ * @license          http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class admin_preset_admin_setting_gradecat_combo extends admin_preset_admin_setting_configselect {
+
+    /**
+     * One db value for two setting attributes
+     *
+     * @param admin_setting $settingdata
+     * @param unknown_type $dbsettingvalue
+     */
+    public function __construct(admin_setting $settingdata, $dbsettingvalue) {
+
+        // The set_attribute_value() method will mod the VARNAME_flag value.
+        $this->attributes = array('forced' => $settingdata->name . '_flag',
+            'adv' => $settingdata->name . '_flag');
+        parent::__construct($settingdata, $dbsettingvalue);
+    }
+
+    /**
+     * Special treatment! the value be extracted from the $value argument
+     */
+    protected function set_visiblevalue() {
+        parent::set_visiblevalue();
+
+        $flagvalue = $this->attributesvalues[$this->settingdata->name . '_flag'];
+
+        if (isset($flagvalue)) {
+
+            if (($flagvalue % 2) == 1) {
+                $forcedvalue = '1';
+            } else {
+                $forcedvalue = '0';
+            }
+
+            if ($flagvalue >= 2) {
+                $advancedvalue = '1';
+            } else {
+                $advancedvalue = '0';
+            }
+            $this->visiblevalue .= $this->delegation->extra_set_visiblevalue($forcedvalue, 'forced');
+            $this->visiblevalue .= $this->delegation->extra_set_visiblevalue($advancedvalue, 'advanced');
+        }
+    }
+}
