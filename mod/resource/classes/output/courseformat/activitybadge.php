870b9148ac44195decab3f23f4208b9422c7c6b1
Sara Arjona <sara@moodle.com>
2023-06-13 12:09:07 +0200

    fixup! MDL-78207 resource: Move file type to activity badge

--- mod/resource/classes/output/courseformat/activitybadge.php
+++ mod/resource/classes/output/courseformat/activitybadge.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace mod_resource\output;
+namespace mod_resource\output\courseformat;
 
 /**
  * Activity badge resource class, used for displaying the file type.
