921255847e3c90fc0c543a5c076fd3190dd7dc11
ferran <ferranrecio@gmail.com>
2025-02-27 13:13:37 +0100

    MDL-83901 mod_workshop: add course overview integration

--- mod/workshop/tests/locallib_test.php
+++ mod/workshop/tests/locallib_test.php
@@ -867,4 +867,77 @@ final class locallib_test extends \advanced_testcase {
 
         return $initialbarprefs;
     }
+
+    /**
+     * Test count_submissions and count_assessments methods.
+     *
+     * @covers \workshop::count_submissions
+     * @covers \workshop::count_assessments
+     */
+    public function test_count_submissions_count_assessments(): void {
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course();
+        $group1 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
+        $group2 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
+        $student1 = $this->getDataGenerator()->create_and_enrol($course, 'student');
+        $student2 = $this->getDataGenerator()->create_and_enrol($course, 'student');
+
+        groups_add_member($group1, $student1);
+        groups_add_member($group2, $student2);
+
+        $activity = $this->getDataGenerator()->create_module(
+            'workshop',
+            ['course' => $course->id , 'groupmode' => SEPARATEGROUPS],
+        );
+        $cm = get_fast_modinfo($course)->get_cm($activity->cmid);
+
+        // Set up a generator to create content.
+        /** @var \mod_workshop_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('mod_workshop');
+
+        // Create some submissions.
+        $submission1id = $generator->create_submission(
+            $activity->id,
+            $student1->id,
+            ['title' => 'My custom title', 'grade' => 85.00000],
+        );
+        $submission2id = $generator->create_submission(
+            $activity->id,
+            $student2->id,
+            ['title' => 'My custom title', 'grade' => null],
+        );
+        // Assess one submission.
+        $generator->create_assessment(
+            $submission1id,
+            $student1->id,
+            ['weight' => 3, 'grade' => 95.00000],
+        );
+        $generator->create_assessment(
+            $submission2id,
+            $student2->id,
+            ['weight' => 3, 'grade' => null],
+        );
+
+        $manager = new workshop($activity, $cm, $course, $cm->context);
+
+        $this->assertEquals(2, $manager->count_submissions());
+        $this->assertEquals(2, $manager->count_submissions('all'));
+        $this->assertEquals(1, $manager->count_submissions($student1->id));
+        $this->assertEquals(1, $manager->count_submissions($student2->id));
+
+        $this->assertEquals(1, $manager->count_submissions('all', $group1->id));
+        $this->assertEquals(1, $manager->count_submissions($student1->id, $group1->id));
+        $this->assertEquals(0, $manager->count_submissions($student2->id, $group1->id));
+
+        $this->assertEquals(2, $manager->count_assessments());
+        $this->assertEquals(1, $manager->count_assessments(true));
+
+        $this->assertEquals(1, $manager->count_assessments(false, $group1->id));
+        $this->assertEquals(1, $manager->count_assessments(true, $group1->id));
+
+        $this->assertEquals(1, $manager->count_assessments(false, $group2->id));
+        $this->assertEquals(0, $manager->count_assessments(true, $group2->id));
+    }
 }
