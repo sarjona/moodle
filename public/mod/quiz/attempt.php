891d35761dfa38347a9aa93e00a465ea124ca66a
Sara Arjona <sara@moodle.com>
2026-06-09 17:51:25 +0200

    MDL-88633 quiz: Hide navigation footer on specific pages

--- public/mod/quiz/attempt.php
+++ public/mod/quiz/attempt.php
@@ -52,6 +52,7 @@ $PAGE->set_url($attemptobj->attempt_url(null, $page));
 $PAGE->set_cacheable(false);
 
 $PAGE->set_secondary_active_tab("modulepage");
+$PAGE->set_show_navigation_footer(false);
 
 // Check login.
 require_login($attemptobj->get_course(), false, $attemptobj->get_cm());
